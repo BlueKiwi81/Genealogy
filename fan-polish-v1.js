2ab9352ab86d3ad26bed7934ace4482f52a0407d
BlueKiwi81 <werner.schrdr@gmail.com>
2026-09-12 20:19:55 +1200

    Load stable fan entrance animation

--- fan-polish-v1.js
+++ fan-polish-v1.js
@@ -1,5 +1,5 @@
 import './person-photos-v1.js';
-import './fan-entrance-v1.js?v=5';
+import './fan-entrance-v1.js?v=6';
 import './fan-era-scaffold-v1.js?v=3';
 
 const treeCanvas = document.getElementById('treeCanvas');
